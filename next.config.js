f02bb0b1720987019aeb8bb63fed20d673100898
Kamikaz07 <pardalfon@gmail.com>
2024-08-07 13:08:29 +0100

    plugins

--- next.config.js
+++ next.config.js
@@ -25,6 +25,10 @@ const nextConfig = withStoreConfig({
         protocol: "https",
         hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
       },
+      {
+        protocol: "https",
+        hostname: "kami-nextjs-ecommerce.s3.eu-west-3.amazonaws.com",
+      },
     ],
   },
 })
